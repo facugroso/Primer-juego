18e05840baacd2e4a22aa0dc8316e4a1fb3846c9
facugroso <110434971+facugroso@users.noreply.github.com>
2023-12-21 10:47:20 -0300

    construyendo el mundo y arreglando algunos errores

--- Juego 1/src/script.js
+++ Juego 1/src/script.js
@@ -1,9 +1,15 @@
-const Phaser = require("phaser");
 
 let config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+        arcade:{
+            gravity: { y: 300},
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -14,16 +20,22 @@ let config = {
 let game = new Phaser.Game(config);
 
 
-function perload() {
+function preload() {
 
     this.load.image('sky', 'assets/sky.png');
-    this.load.image('platform', 'assets/platform.png');
-    this.load.image('platform', 'assets/platform.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
     this.load.image('bomb', 'assets/bomb.png');
     this.load.image('dude', 'assets/dude.png', {frameWidth: 32, frameHeight:48});
 
 }
+function create(){
+    this.add.image(400, 300, 'sky');
 
+    platforms = this.physics.add.staticGroup();
+
+   platforms.create(400, 568, 'ground').setScale(2);
+}
 function update() {
     
 }

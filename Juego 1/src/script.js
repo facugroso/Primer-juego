bc4f4a5e132b2cd2edf15facb901398fc291554f
facugroso <110434971+facugroso@users.noreply.github.com>
2023-12-26 09:54:24 -0300

    aviso al sistema de la creacion de las plataformas y apuntes

--- Juego 1/src/script.js
+++ Juego 1/src/script.js
@@ -31,10 +31,12 @@ function preload() {
 }
 function create(){
     this.add.image(400, 300, 'sky');
-
+    //crea un nuevo grupo de elementos estàticos y los agrega a la variable local platforms.
+    //cuerpo dinámico: cuerpo que si le aplico fuerza, se mueve, puede chocar con otros elementos
+    //cuerpo estàtico: siempre tiene una posición y un tamaño. Cuando algo choca contra el no se mueve.
     platforms = this.physics.add.staticGroup();
 
-   platforms.create(400, 568, 'ground').setScale(2);
+   platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 
    platforms.create(600, 400, 'ground');
    platforms.create(100, 290, 'ground');
